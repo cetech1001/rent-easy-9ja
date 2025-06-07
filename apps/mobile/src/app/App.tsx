6567a652a9aae94ab0d80d3030a8585adb4d038b
Chiemerie <alexemerie7@gmail.com>
2025-06-07 14:03:22 +0100

    Made a lot of config changes

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import "../../global.css"
+import 'react-native-gesture-handler';
 import {DarkTheme, DefaultTheme, NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {useColorScheme} from "react-native";
@@ -7,6 +8,7 @@ import {FLOWS, ROUTES} from "../routes";
 import {OnboardingScreen} from "../components/onboarding/onboarding";
 import {AuthFlow} from "../components/auth/auth";
 import {TenantFlow} from "../components/tenant/tenant";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const RootStack = createNativeStackNavigator();
 
@@ -14,13 +16,15 @@ export const App = () => {
   const colorScheme = useColorScheme();
 
   return (
-    <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootStack.Navigator initialRouteName={FLOWS.tenantFlow}
-                           screenOptions={{ headerShown: false }}>
-        <RootStack.Screen name={ROUTES.onboarding} component={OnboardingScreen}/>
-        <RootStack.Screen name={FLOWS.authFlow} component={AuthFlow}/>
-        <RootStack.Screen name={FLOWS.tenantFlow} component={TenantFlow}/>
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <RootStack.Navigator initialRouteName={ROUTES.onboarding}
+                             screenOptions={{ headerShown: false }}>
+          <RootStack.Screen name={ROUTES.onboarding} component={OnboardingScreen}/>
+          <RootStack.Screen name={FLOWS.authFlow} component={AuthFlow}/>
+          <RootStack.Screen name={FLOWS.tenantFlow} component={TenantFlow}/>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
